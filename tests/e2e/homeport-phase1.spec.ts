500227c43eb36f6a7d935710839b4b3748a2b692
Gray <kato.gray@gwplastics.com>
2026-08-29 21:28:36 -0400

    test: use policy-safe Homeport fixture passwords

--- tests/e2e/homeport-phase1.spec.ts
+++ tests/e2e/homeport-phase1.spec.ts
@@ -6,7 +6,7 @@ import { hash } from "bcryptjs";
 import { db } from "../../src/lib/db";
 import { registerAccount } from "../../src/wayfarer/accounts";
 
-const password = "Homeport-validation-passphrase-2026";
+const password = "Signal-quartz-compass-2026";
 const baseURL = process.env.PLAYWRIGHT_BASE_URL ?? "http://127.0.0.1:3100";
 const evidenceRoot = process.env.HOMEPORT_PHASE1_EVIDENCE_ROOT
   ? path.resolve(process.env.HOMEPORT_PHASE1_EVIDENCE_ROOT)
